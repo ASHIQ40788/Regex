0b76beb0bad567b3335e685087215a4f1140cc2e
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-22 14:30:38 +0530

    [ASHIQ] added : Addition for implement UnitTestProject

---
+++ Addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RegexDemo058
+{
+    public class Addition
+    {
+        public int Sum(int first,int second)
+        {
+            int res = Multi(first);
+            return first + res;
+        }
+        public int Multi(int first)
+        {
+            return first * first;
+        }
+    }
+}
